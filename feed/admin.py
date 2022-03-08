d7bd8ea4a34917767c36c6838c8458bb8efa3500
Andras Bodrogai <2575706B@student.gla.ac.uk>
2022-03-08 14:56:55 +0000

    populating and database update - potential fix to population script

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from feed.models import Category,Post,UserProfile,Comment
 
+admin.site.register(Category)
+admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Comment)
 # Register your models here.
+
