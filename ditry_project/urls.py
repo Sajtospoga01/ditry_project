b8648cd96085bab0875102d0ce2a2866f6c227e7
Andras Bodrogai <2575706B@student.gla.ac.uk>
2022-03-08 14:57:25 +0000

    populating and database update - potential fix to population script

--- ditry_project/urls.py
+++ ditry_project/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    #path('feed/', include('feed.urls')),
+    path('', views.home, name='home'),
+    path('feed/', include('feed.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
