67761afd646bf9cf7abe02cde7000d0d914312fb
Anna-Sophia Bohn <2578025b@student.gla.ac.uk>
2022-03-13 11:18:13 +0000

    two different search views for search user and search category+updated urls

--- feed/urls.py
+++ feed/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     # user_name cannot be 'register', 'login', 'logout', 'help', etc.
     path('<str:username>/', views.show_user, name='account'),
     path('<str:username>/folders/', views.all_folders, name='all_folders'),
-    path('<str:username>/folders/add-folder/', views.add_folders, name='add_folders'),
+    path('<str:username>/folders/add-folder/', views.add_folder, name='add_folder'),
     path('<str:username>/folders/<slug:folder_name_slug>/', views.show_folder, name='show_folder'),
     path('<str:username>/folders/<slug:folder_name_slug>/delete-folder/', views.delete_folder, name='delete_folder'),
     path('<str:username>/folders/<slug:folder_name_slug>/<int:post_id>/del-saved-post/', views.delete_saved_post,
@@ -37,16 +37,12 @@ urlpatterns = [
     path('<str:username>/my-attempts/', views.show_my_attempts, name='show_my_attempts'),
     path('<str:username>/my-posts/<int:post_id>/delete-post/', views.delete_post, name='delete_post'),
     path('<str:username>/update-profile/', views.update_profile,name='update_profile'),
-    path('<str:username>/all-followed-categories/', views.all_followed_categories,name='all_followed_categories'),
-    
 
     path('follow-user/<str:username>/', views.follow_user, name='follow_user'),
     path('follow-category/<slug:category_slug>/', views.follow_category, name='follow_category'),
     
-    path('search/', views.search, name = 'search'),
+    path('search-title/', views.search_title, name = 'search_title'),
+    path('search-user/', views.search_user, name = 'search_user'),
     
     path('add-post-to-category/', views.add_post_to_category, name='add_post_to_category'),
-    path('<slug:category_slug>/', views.show_category, name='show_category'),
-
-
 ]
