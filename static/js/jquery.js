12491d4f0de45e3be66aa0e3c66c66bc14a84e43
Arron Curry <2577001c@student.gla.ac.uk>
2022-03-17 23:39:45 +0000

    added jquery - toggle heart buttons

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -2,22 +2,48 @@ $(document).ready(function() {
 	
 	// toggles whether heart button is liked/hearted
 	function toggleLiked(button) {
-		var unliked = 'images/heart2.png';
-		var liked = 'images/hearted.png';
-		
-		if(button.src='images/heart2.png'){
-			button.src='images/hearted.png'
-			alert("second")
+		var unliked = "/static/images/heart2.png";
+		var liked = "/static/images/hearted.png";
+
+		if(button.getAttribute('src') == unliked){
+			button.setAttribute('src', liked)
+			alert("liked")
 		}else{
-			button.src='images/heart2.png'
-			alert("third")
+			button.setAttribute('src', unliked)
+			alert("unliked")
 		}
 	}
 	
 	$('#sampleHeart1').click(function() {
-		toggleLiked();
+		toggleLiked(document.getElementById('sampleHeart1'));
 	});
 	
+	$('#sampleHeart2').click(function() {
+		toggleLiked(document.getElementById('sampleHeart2'));
+	});
 	
+	$('#sampleHeart3').click(function() {
+		toggleLiked(document.getElementById('sampleHeart3'));
+	});
+
+	$('#sampleHeart4').click(function() {
+		toggleLiked(document.getElementById('sampleHeart4'));
+	});	
+
+	$('#sampleHeart5').click(function() {
+		toggleLiked(document.getElementById('sampleHeart5'));
+	});	
+
+	$('#sampleHeart6').click(function() {
+		toggleLiked(document.getElementById('sampleHeart6'));
+	});
+
+	$('#sampleHeart7').click(function() {
+		toggleLiked(document.getElementById('sampleHeart7'));
+	});
+
+	$('#sampleHeart8').click(function() {
+		toggleLiked(document.getElementById('sampleHeart8'));
+	});
 	
 })
