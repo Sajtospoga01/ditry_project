a566ffb5e8fd5aa5f26c1d25bb3ffc92d1dfad24
Arron Curry <2577001c@student.gla.ac.uk>
2022-03-20 22:28:00 +0000

    toggleLiked now uses post id

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,56 +1,31 @@
 $(document).ready(function() {
 	
 	// toggles whether heart button is liked/hearted
-	function toggleLiked(button) {
-		var unliked = "/static/images/heart2.png";
-		var liked = "/static/images/hearted.png";
+	
+	
 
-		if(button.getAttribute('src') == unliked){
-			button.setAttribute('src', liked)
-			alert("liked")
-		}else{
-			button.setAttribute('src', unliked)
-			alert("unliked")
-		}
-	}
 	
 	function like(button) {
 
 		
 	}
 	
-	$('#sampleHeart1').click(function() {
-		toggleLiked(document.getElementById('sampleHeart1'));
-	});
-	
-	$('#sampleHeart2').click(function() {
-		toggleLiked(document.getElementById('sampleHeart2'));
-	});
-	
-	$('#sampleHeart3').click(function() {
-		toggleLiked(document.getElementById('sampleHeart3'));
-	});
-
-	$('#sampleHeart4').click(function() {
-		toggleLiked(document.getElementById('sampleHeart4'));
-	});	
-
-	$('#sampleHeart5').click(function() {
-		toggleLiked(document.getElementById('sampleHeart5'));
-	});	
-
-	$('#sampleHeart6').click(function() {
-		toggleLiked(document.getElementById('sampleHeart6'));
-	});
-
-	$('#sampleHeart7').click(function() {
-		toggleLiked(document.getElementById('sampleHeart7'));
-	});
-
-	$('#sampleHeart8').click(function() {
-		toggleLiked(document.getElementById('sampleHeart8'));
-	});
-	
 	
 	
 })
+
+
+function toggleLiked(postId) {
+		var unliked = "/static/images/heart2.png";
+		var liked = "/static/images/hearted.png";
+
+		var id = document.getElementById(postId);
+		
+		if(id.getAttribute('src') == unliked){
+			id.setAttribute('src', liked)
+			alert("liked")
+		}else{
+			id.setAttribute('src', unliked)
+			alert("unliked")
+		}
+	}
