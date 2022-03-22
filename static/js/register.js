b3272ed86c31c92bdfa7dd91ecfb88aa8bf0216d
Xiaoyang Bao <2575138b@student.gla.ac.uk>
2022-03-22 05:30:38 +0000

    Regiser Second edit

---
+++ static/js/register.js
@@ -0,0 +1,9 @@
+var form_fields = document.getElementsByTagName('input')
+form_fields[1].placeholder = 'XXXX'
+form_fields[2].placeholder = '123456@789.com'
+form_fields[3].placeholder = 'e.g. Lebron'
+form_fields[4].placeholder = 'e.g. James'
+form_fields[5].placeholder = '12345678abc'
+form_fields[6].placeholder = '12345678abc'
+
+
