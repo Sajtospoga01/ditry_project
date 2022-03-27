f2fb2e52767cbf7accc9d8b016642cb0e0dff4c1
Sajtospoga01 <abodrogai@gmail.com>
2022-03-27 15:18:47 +0100

    minor update to views

--- feed/views.py
+++ feed/views.py
@@ -74,7 +74,7 @@ def follow_category(request, category_name_slug):
 def show_my_attempts(request,username):
     user = UserProfile.objects.get(username=username)
     my_attempts = Queries.get_user_attempts(user.id)
-    return render(request, 'feed/attempts.html', {'posts': tableify(my_attempts), 'user': user})
+    return render(request, 'feed/attempts.html', {'posts': my_attempts, 'user': user})
 
 
 @login_required
