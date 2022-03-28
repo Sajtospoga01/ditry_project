4bd086b524a6245b3e36e0fe1ab9b18e3d2b357e
ahosselluni <98162587+ahosselluni@users.noreply.github.com>
2022-03-28 13:43:28 +0100

    Update wsgi.py
    i don't know if this works

--- ditry_project/wsgi.py
+++ ditry_project/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = 'home/ditry/ditry'
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ditry_project.settings')
+os.chdir(path)
 
-application = get_wsgi_application()
+os.environ['DJANGO_SETTINGS_MODULE']='ditry.settings'
+
+import django
+django.setup()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
