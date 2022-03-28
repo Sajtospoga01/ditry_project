453bd3ff27305fb280f309365a3f23f9dd5ff507
ahosselluni <98162587+ahosselluni@users.noreply.github.com>
2022-03-28 15:05:24 +0100

    back to initial i had no idea what i was doing

--- ditry_project/wsgi.py
+++ ditry_project/wsgi.py
@@ -8,15 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
-path = 'home/ditry/ditry_project'
-if path not in sys.path:
-    sys.path.append(path)
+from django.core.wsgi import get_wsgi_application
 
-os.chdir(path)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ditry_project.settings')
 
-os.environ['DJANGO_SETTINGS_MODULE']='ditry_project.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
